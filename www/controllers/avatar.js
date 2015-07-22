ee298e020d54ce4b6db0cc20d8c4c8cc16dc69a3
markiiikram <marki.sveen@gmail.com>
2015-07-21 20:02:25 -0400

    avatar page start

--- www/controllers/avatar.js
+++ www/controllers/avatar.js
@@ -1,17 +1,17 @@
 .controller('AvatarCtrl', function($scope) {
 
 $scope.avatars = [
-  boy1/boy1_0.svg,
-  boy2/boy2_0.svg,
-  boy3/boy3_0.svg,
-  boy4/boy4_0.svg,
-  boy5/boy5_0.svg,
-  girl1/girl1_0.svg,
-  girl2/girl2_0.svg,
-  girl3/girl3_0.svg,
-  girl4/girl4_0.svg,
-  girl5/girl5_0.svg,
-  girl6/girl6_0.svg
+  '../img/boy1/boy1_0.svg',
+  '../img/boy2/boy2_0.svg',
+  '../img/boy3/boy3_0.svg',
+  '../img/boy4/boy4_0.svg',
+  '../img/boy5/boy5_0.svg',
+  '../img/girl1/girl1_0.svg',
+  '../img/girl2/girl2_0.svg',
+  '../img/girl3/girl3_0.svg',
+  '../img/girl4/girl4_0.svg',
+  '../img/girl5/girl5_0.svg',
+  '../img/girl6/girl6_0.svg'
 ]
 
 
