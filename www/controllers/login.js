2036a75b02556f9f27a40ea11e9c5e9942bd1484
markiiikram <marki.sveen@gmail.com>
2015-08-04 16:44:06 -0400

    started adding angular validation to login

--- www/controllers/login.js
+++ www/controllers/login.js
@@ -2,4 +2,19 @@ angular.module('911-heroes.controllers', [])
 
 .controller('LoginCtrl', function($scope) {
 
+
+  $scope.submitForm = function(isValid){
+
+    $scope.submitted = true;
+
+    if(isValid) {
+      console.log('party');
+    }
+
+    else {
+      console.log('you fail');
+    }
+
+  };
+
 });
