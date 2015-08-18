2d3b504c865a2e1986fad83bd7641f944dc48d42
Nitin Malik <malik.nit@gmail.com>
2015-08-18 18:14:06 -0400

    Add onNext() function

--- www/controllers/login.js
+++ www/controllers/login.js
@@ -1,14 +1,15 @@
 angular.module('911-heroes.controllers', [])
 
-.controller('LoginCtrl', function($scope) {
+.controller('LoginCtrl', function($scope, stateService) {
 
 
   $scope.submitForm = function(isValid){
-
+  	
     $scope.submitted = true;
 
     if(isValid) {
       console.log('party');
+      stateService.onNext()
     }
 
     else {
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('911-heroes', ['ionic', '911-heroes.controllers'])
+angular.module('911-heroes', ['ionic', '911-heroes.controllers', '911-heroes.services', '911-heroes.constants'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
