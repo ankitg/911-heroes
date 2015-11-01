b61a7073d573b9701dfea6411ed0fe9b8b7e5d83
Nitin Malik <malik.nit@gmail.com>
2015-11-01 15:10:59 -0500

    hooked up timeOut page

--- www/controllers/timeOut.js
+++ www/controllers/timeOut.js
@@ -1,5 +1,10 @@
 angular.module('911-heroes.controllers', [])
 
-.controller('TimeOutCtrl', function($scope) {
+.controller('TimeOutCtrl', function($scope, $state, stateService) {
 
+    $scope.onButtonPressed = function () {
+
+      var prevPhaseNavLocation = stateService.getNavLocationForPreviousPhase();
+      $scope.goToNavLocation(prevPhaseNavLocation);
+    }
 });
