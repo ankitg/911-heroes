0df3a43df9d224f712d386901d03ff3eeb799516
Ankit Guglani <ankitg@gmail.com>
2015-11-15 21:22:49 -0500

    • Updated Module 4 with basic Module 2 logic

--- www/controllers/module4.js
+++ www/controllers/module4.js
@@ -8,9 +8,13 @@ angular.module('911-heroes.controllers', [])
     $scope.module2 = false;
     $scope.module3 = false;
 
+    chooseScenario();
+  }
+
+  var chooseScenario = function() {
     var chosenScenarioCategoryIndex= Math.floor(Math.random() * 4);
     $scope.scenario = SCENARIOS[chosenScenarioCategoryIndex][Math.floor(Math.random() * SCENARIOS[chosenScenarioCategoryIndex].length)];
-  }
+  };
 
   module1();
 
@@ -27,13 +31,51 @@ angular.module('911-heroes.controllers', [])
     $scope.module1 = false;
     $scope.module2 = true;
     $scope.module3 = false;
+
     $scope.lockscreen = true;
   }
 
+  $scope.goToDialpad = function() {
+    $scope.lockscreen = false;
+    clearKeypad();
+  };
+
+  var keyCount = 0;
+  var clearKeypad = function() {
+    $scope.dialedNumber = "";
+    keyCount = 0;
+  };
+
+  $scope.dialPadKeyPressed = function(key) {
+    keyCount++;
+
+    // Update the dial screen.
+    if(key === "bksp") {
+      $scope.dialedNumber = $scope.dialedNumber.slice(0, - 1);
+    } else if(key === "call") {
+      if($scope.dialedNumber === "911") {
+        if(keyCount === 4) {
+          module3();
+        } else {
+          module2();
+        }
+      } else {
+        module2();
+      }
+    } else if(key === "contacts") {
+      console.log("Sorry. You have no friends. :(");
+    } else {
+      $scope.dialedNumber += key;
+    }
+  };
+
   //module 3
-  $scope.module3 = false;
-  $scope.video   = true;
-  $scope.dialpad = false;
-  $scope.calling = false;
+  function module3() {
+    $scope.module1 = false;
+    $scope.module2 = false;
+    $scope.module3 = true;
+
+    $scope.calling = true;
+  }
 
 });
