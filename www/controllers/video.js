cd5ef4680baad0106007e27fd57959786d44356a
markiiikram <marki.sveen@gmail.com>
2015-12-02 15:53:03 -0500

    clapper added to video page

--- www/controllers/video.js
+++ www/controllers/video.js
@@ -8,18 +8,40 @@ angular.module('911-heroes.controllers', [])
     $scope.clapperboard = false;
   };
 
+  var clapperTop = document.getElementById('clapperTop');
+  var clapperBottom = document.getElementById('clapperBottom');
+
+  function clapperDown() {
+    clapperTop.classList.add('clapper-down');
+  };
+
+  function clapperRemove() {
+    clapperTop.classList.add('display-none');
+    clapperBottom.classList.add('display-none');
+  }
+
+  var clapper = function() {
+    clapperTop.classList.add('clapper-up');
+    setInterval(clapperDown, 1000);
+    setInterval(clapperRemove, 2500);
+    setInterval(playVideo, 2500);
+  }
+
+
   var video = undefined; // Setting only instance of the variable, lighter on memory. ^_^
+  
   var playVideo = function() {
     video = document.getElementById('videoScreen');
     video.play();
     video.onended = function() { console.log("ENABLE ALL THE BUTTONS NOW!"); }; // Enable the buttons after video has ended
   };
+
   var stopVideo = function() {
     video = document.getElementById('videoScreen');
     video.pause();
   };
 
-  $scope.playAudio("ModelingVideo.mp3", null, playVideo);
+  $scope.playAudio("ModelingVideo.mp3", null, clapper);
 
   $scope.playVideo = function() {
     playVideo();
