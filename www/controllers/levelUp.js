363965d551e58a3b64076ae0e4fad644c1c4e856
Nitin Malik <nitin.malik@digiflare.com>
2015-12-06 13:37:04 -0500

    Fix: Audio was not playing on the "level up" screen

--- www/controllers/levelUp.js
+++ www/controllers/levelUp.js
@@ -1,21 +1,27 @@
-angular.module('911-heroes.controllers', [])
+var controllers = angular.module('911-heroes.controllers', []);
 
-.controller('LevelUpCtrl', function($scope) {
+controllers.controller('LevelUpCtrl', function($scope) {
 
-   $scope.phase_1 = $scope.phase_2 = $scope.level_up = false;
+  $scope.$on('$ionicView.enter', function() {
+    proceed();
+  });
 
-  if($scope.currentPhase === "M1P1" || $scope.currentPhase === "M2P1" || $scope.currentPhase === "M3P1") {
-  	$scope.phase_1 = true;
-  	$scope.playAudio("Congratulations.mp3", null, goToNextWithDelay());
-  } else if($scope.currentPhase === "M1P2" || $scope.currentPhase === "M2P2") {
-  	$scope.phase_2 = true;
-  	$scope.playAudio("Congratulations.mp3", null, goToNextWithDelay());
-  } else if($scope.currentPhase === "M1P3" || $scope.currentPhase === "M2P3" || $scope.currentPhase === "M3P2") {	
-    $scope.level_up = true; 
-    $scope.playAudio("Congratulations.mp3", null, goToNextWithDelay());
-  } else if($scope.currentPhase == "M4P1") {
-//  	$scope.level_up = true; 
-    $scope.playAudio("FinalCongratulations.mp3");
+  function proceed() {
+    $scope.phase_1 = $scope.phase_2 = $scope.level_up = false;
+
+    if($scope.currentPhase === "M1P1" || $scope.currentPhase === "M2P1" || $scope.currentPhase === "M3P1") {
+      $scope.phase_1 = true;
+      $scope.playAudio("Congratulations.mp3", null, goToNextWithDelay);
+    } else if($scope.currentPhase === "M1P2" || $scope.currentPhase === "M2P2") {
+      $scope.phase_2 = true;
+      $scope.playAudio("Congratulations.mp3", null, goToNextWithDelay);
+    } else if($scope.currentPhase === "M1P3" || $scope.currentPhase === "M2P3" || $scope.currentPhase === "M3P2") {
+      $scope.level_up = true;
+      $scope.playAudio("Congratulations.mp3", null, goToNextWithDelay);
+    } else if($scope.currentPhase == "M4P1") {
+//  	$scope.level_up = true;
+      $scope.playAudio("FinalCongratulations.mp3");
+    }
   }
 
   function goToNextWithDelay(delay) {
@@ -25,5 +31,4 @@ angular.module('911-heroes.controllers', [])
 
     setTimeout($scope.goToNext, delayDuration);
   }
-
 });
