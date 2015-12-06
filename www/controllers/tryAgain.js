f2c5282a70fc744203c1d04231c1ecd5e7398ff0
Nitin Malik <nitin.malik@digiflare.com>
2015-12-06 17:59:55 -0500

    added "try again" animation

--- www/controllers/tryAgain.js
+++ www/controllers/tryAgain.js
@@ -1,8 +1,29 @@
 var controllers = angular.module('911-heroes.controllers', []);
 
-controllers.controller('TryAgainCtrl', function($scope, stateService) {
+controllers.controller('TryAgainCtrl', function($scope, stateService, $timeout) {
 
-  $scope.practice_again = true;
+  $scope.practiceTextStyle = {'visibility': 'hidden'};
+  $scope.practice_again = false;
+
+  var timeout = null;
+
+  $scope.$on('$ionicView.enter', function() {
+
+    timeout = $timeout(animatePracticeAgain, 1000);
+  });
+
+  $scope.$on('$ionicView.leave', function() {
+
+    if(timeout) {
+      $timeout.cancel(timeout);
+    }
+  });
+
+  function animatePracticeAgain() {
+    $scope.practice_again = true;
+
+    $scope.practiceTextStyle = {'visibility': 'visible'};
+  }
 
   $scope.onButtonPressed = function () {
 
