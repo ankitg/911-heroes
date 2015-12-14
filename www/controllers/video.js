b7eb3bfaa020bf212507d6a48e90c48e3706beb1
Nitin Malik <nitin.malik@digiflare.com>
2015-12-13 19:38:30 -0500

    - added a dev mode for convenience during development - modified clapboard animation to be faster - handle enabling/disabling of the continue button on the video page

--- www/controllers/video.js
+++ www/controllers/video.js
@@ -1,12 +1,12 @@
-angular.module('911-heroes.controllers', [])
+var controllers = angular.module('911-heroes.controllers', []);
 
-.controller('VideoCtrl', function($scope) {
+controllers.controller('VideoCtrl', function($scope) {
 
-  $scope.clapperboard = true;
+  $scope.isContinueEnabled = false;
 
-  $scope.hideClapperboard = function() {
-    $scope.clapperboard = false;
-  };
+  $scope.$on('$ionicView.leave', function() {
+    stopVideo();
+  });
 
   var clapperTop = document.getElementById('clapperTop');
   var clapperBottom = document.getElementById('clapperBottom');
@@ -18,22 +18,36 @@ angular.module('911-heroes.controllers', [])
   function clapperRemove() {
     clapperTop.classList.add('display-none');
     clapperBottom.classList.add('display-none');
-  }
+  };
 
   var clapper = function() {
-    clapperTop.classList.add('clapper-up');
-    setInterval(clapperDown, 1000);
-    setInterval(clapperRemove, 2500);
-    setInterval(playVideo, 2500);
-  }
 
+    var downTime = 250;
+    var removeTime = downTime + 1000;
+    var playTime = removeTime;
+
+    clapperTop.classList.add('clapper-up');
+    setTimeout(clapperDown, downTime);
+    setTimeout(clapperRemove, removeTime);
+    setTimeout(playVideo, playTime);
+  };
 
   var video = undefined; // Setting only instance of the variable, lighter on memory. ^_^
-  
+
   var playVideo = function() {
     video = document.getElementById('videoScreen');
+    if(!video) {
+      return;
+    }
+
     video.play();
-    video.onended = function() { console.log("ENABLE ALL THE BUTTONS NOW!"); }; // Enable the buttons after video has ended
+    video.onended = function() {
+
+      // Enable the buttons after video has ended
+      $scope.$apply(function() {
+        $scope.isContinueEnabled = true;
+      });
+    };
   };
 
   var stopVideo = function() {
@@ -44,12 +58,20 @@ angular.module('911-heroes.controllers', [])
   $scope.playAudio("ModelingVideo.mp3", null, clapper);
 
   $scope.playVideo = function() {
+    video = document.getElementById('videoScreen');
+    if (!video) {
+      return;
+    }
     playVideo();
   };
 
   $scope.continue = function() {
+
+    if (!$scope.isContinueEnabled && !$scope.isDevMode) {
+      return;
+    }
+
     stopVideo();
     $scope.goToNext();
   };
-
 });
